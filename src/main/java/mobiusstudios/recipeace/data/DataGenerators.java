33a5cefca08e4539420413fae26bad67e226c69e
MobiusDT <mobiusdt@gmail.com>
2021-01-18 00:22:57 -0500

    add ModItemModelProvider to DataGenerators

--- src/main/java/mobiusstudios/recipeace/data/DataGenerators.java
+++ src/main/java/mobiusstudios/recipeace/data/DataGenerators.java
@@ -1,9 +1,11 @@
 package mobiusstudios.recipeace.data;
 
 import mobiusstudios.recipeace.Recipeace;
+import mobiusstudios.recipeace.data.client.ModItemModelProvider;
 import mobiusstudios.recipeace.data.loot.ModLootModifierProvider;
 import mobiusstudios.recipeace.data.recipes.ModRecipeProvider;
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -16,7 +18,9 @@ public final class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
+        generator.addProvider(new ModItemModelProvider(generator, existingFileHelper));
         generator.addProvider(new ModRecipeProvider(generator));
         generator.addProvider(new ModLootModifierProvider(generator));
     }
