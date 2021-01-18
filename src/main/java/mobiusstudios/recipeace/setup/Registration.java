0ece39ecb49ccce54b11ae277675e47a118a4572
MobiusDT <mobiusdt@gmail.com>
2021-01-18 00:35:08 -0500

    remove unused RecipeSerializer stuff from Registration

--- src/main/java/mobiusstudios/recipeace/setup/Registration.java
+++ src/main/java/mobiusstudios/recipeace/setup/Registration.java
@@ -2,7 +2,6 @@ package mobiusstudios.recipeace.setup;
 
 import mobiusstudios.recipeace.Recipeace;
 import net.minecraft.item.Item;
-import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,12 +11,10 @@ import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class Registration {
     public static final DeferredRegister<Item> ITEMS = create(ForgeRegistries.ITEMS);
-    public static final DeferredRegister<IRecipeSerializer<?>> RECIPE_SERIALIZERS = create(ForgeRegistries.RECIPE_SERIALIZERS);
 
     public static void register() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ITEMS.register(modEventBus);
-        RECIPE_SERIALIZERS.register(modEventBus);
 
         ModItems.register();
     }
