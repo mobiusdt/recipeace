fa3e30b211a87bc27b7cd4f50645c6eb0ce8aacb
MobiusDT <mobiusdt@gmail.com>
2021-01-18 00:19:46 -0500

    add gold_rod texture

--- src/main/java/mobiusstudios/recipeace/data/client/ModItemModelProvider.java
+++ src/main/java/mobiusstudios/recipeace/data/client/ModItemModelProvider.java
@@ -1,8 +1,11 @@
 package mobiusstudios.recipeace.data.client;
 
 import mobiusstudios.recipeace.Recipeace;
+import mobiusstudios.recipeace.data.client.items.GoldRod;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class ModItemModelProvider extends ItemModelProvider {
@@ -12,6 +15,10 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
+        ModelFile itemGenerated = getExistingFile(new ResourceLocation("item/generated"));
 
+        getBuilder(GoldRod.UNLOCALIZED_NAME)
+                .parent(itemGenerated)
+                .texture("layer0", GoldRod.TEXTURE_PATH_0);
     }
 }
