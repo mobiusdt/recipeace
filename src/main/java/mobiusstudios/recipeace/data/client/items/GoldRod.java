9d60ffbb53f957bbefa62550147ec45e1cf7dec7
MobiusDT <mobiusdt@gmail.com>
2021-01-18 00:22:34 -0500

    fix gold rod texture path

--- src/main/java/mobiusstudios/recipeace/data/client/items/GoldRod.java
+++ src/main/java/mobiusstudios/recipeace/data/client/items/GoldRod.java
@@ -6,6 +6,6 @@ import net.minecraft.item.Items;
 
 public class GoldRod {
     public static final String UNLOCALIZED_NAME = "gold_rod";
-    public static final String TEXTURE_PATH_0 = "items/gold_rod";
+    public static final String TEXTURE_PATH_0 = "item/gold_rod";
     public static final ItemGroup ITEM_GROUP = Recipeace.ITEM_GROUP;
 }
