03cc766ced78eb0fe14151ce740d338c5ad6290d
MobiusDT <mobiusdt@gmail.com>
2021-01-19 00:21:06 -0500

    initial outline of BoneLootModifier

---
+++ src/main/java/mobiusstudios/recipeace/data/loot/BoneLootModifier.java
@@ -0,0 +1,36 @@
+package mobiusstudios.recipeace.data.loot;
+
+import com.google.gson.JsonObject;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
+import net.minecraft.loot.conditions.ILootCondition;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.common.loot.LootModifier;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public class BoneLootModifier extends LootModifier {
+    public BoneLootModifier(ILootCondition[] conditionsIn) {
+        super(conditionsIn);
+    }
+    @Nonnull
+    @Override
+    protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+        return null;
+    }
+
+    public static class Serializer extends GlobalLootModifierSerializer<BoneLootModifier> {
+
+        @Override
+        public BoneLootModifier read(ResourceLocation location, JsonObject object, ILootCondition[] ailootcondition) {
+            return new BoneLootModifier(ailootcondition);
+        }
+
+        @Override
+        public JsonObject write(BoneLootModifier instance) {
+            return makeConditions(instance.conditions);
+        }
+    }
+}
